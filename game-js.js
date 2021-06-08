45ec52a8730041bb81756df2f2010cbeef233c24
TarasovIlia <tarasov002@gmail.com>
2021-06-09 01:25:16 +0300

    done

--- game-js.js
+++ game-js.js
@@ -1,1 +1,15 @@
-alert ("hello");
+const player = document.querySelector('.player');
+const block = document.querySelector('.block');
+
+document.addEventListener("keydown", function(event){
+    jump();
+});
+
+function jump () {
+    if (player.classList != "jump"){
+        player.classList.add("jump")
+    }
+    setTimeout( function (){
+        player.classList.remove("jump")
+    }, 1000)
+}
