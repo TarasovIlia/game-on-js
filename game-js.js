e3aa7a7ccb64731ac01e0c6f8df13da71f029151
TarasovIlia <tarasov002@gmail.com>
2021-06-09 01:44:24 +0300

    fuck

--- game-js.js
+++ game-js.js
@@ -1,15 +1,27 @@
 const player = document.querySelector('.player');
 const block = document.querySelector('.block');
 
-document.addEventListener("keydown", function(event){
-    jump();
+// document.addEventListener("keydown", function(event){
+//     jump();
+// });
+
+// function jump () {
+//     if (player.classList != "jump"){
+//         player.classList.add("jump")
+//     }
+//     setTimeout( function (){
+//         player.classList.remove("jump")
+//     }, 1000)
+// }
+
+let offset = 0;
+
+document.querySelector('.player').addEventListener('click', function(){
+    offset = offset + 100;
+    player.style.left = offset + 'px'
 });
 
-function jump () {
-    if (player.classList != "jump"){
-        player.classList.add("jump")
-    }
-    setTimeout( function (){
-        player.classList.remove("jump")
-    }, 1000)
-}
+document.addEventListener("keydown", function(){
+    offset = offset - 100;
+    player.style.left = offset + 'px'
+});
